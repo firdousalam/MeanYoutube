ce74aa5d2efbfa69df50f86140ceabdec5a39f8a
firdous alam <firdous.alam2058@gmail.com>
2023-01-17 16:28:09 +0530

    Nodejs Hello world

---
+++ frontend/src/app/post/postcreate.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';  
+  
+@Component({  
+  selector: 'app-post-create',  
+  templateUrl: './postcreate.component.html',  
+})  
+export class PostCreateComponent {  
+    
+}  
